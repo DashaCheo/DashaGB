b44672ce324e07918c5bdb17b73bed69be1da79a
DashaCheo <chernyshevadashaa@yandex.ru>
2021-02-10 18:06:27 +0400

    ДЗ

--- src/HW1.java
+++ src/HW1.java
@@ -1,6 +1,22 @@
 public class HW1 {
 
-    void foo() {
-        System.out.println("Dasha");
+    private static int calculate(int a, int b, int c, int d) {
+        return a * (b + (c / d));
+    }
+
+    private static boolean between10and20(int x, int y) {
+        int sum = x + y;
+        return sum >= 10 && sum <= 20;
+    }
+
+    private static boolean positiveOrNegative(int num) {
+        return num >= 0;
+    }
+
+    private static boolean isNegative(int number) {
+        return number < 0;
+    }
+    private static String Hello(String name) {
+        return "Привет, Dasha";
     }
 }
