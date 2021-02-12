bbf71ffca57c9e5e7b054ff1db7aaad6afdc0760
DashaCheo <chernyshevadashaa@yandex.ru>
2021-02-12 09:54:12 +0400

    ДЗ 2, 5 задание сделала с интернетом, но поняла как делать)

--- src/HW1.java
+++ src/HW1.java
@@ -1,22 +1,49 @@
 public class HW1 {
 
-    private static int calculate(int a, int b, int c, int d) {
-        return a * (b + (c / d));
+    private static void invertArrayVar1(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (arr[i] == 1) ? 0 : 1;
+        }
     }
 
-    private static boolean between10and20(int x, int y) {
-        int sum = x + y;
-        return sum >= 10 && sum <= 20;
-    }
+    private static void changeArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] *= 2;
+            }
+        }
 
-    private static boolean positiveOrNegative(int num) {
-        return num >= 0;
-    }
+        private static void fillDiagonal1 ( int[][] arr){
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0, z = arr.length - 1; j < arr.length; j++, z--) {
+                    if (i == j || i == z) {
+                        arr[i][j] = 1;
+                    } else {
+                        arr[i][j] = 0;
+                    }
+                }
+            }
+        }
+        private static int findMax ( int[] arr){
+            int max = arr[0];
 
-    private static boolean isNegative(int number) {
-        return number < 0;
-    }
-    private static String Hello(String name) {
-        return "Привет, Dasha";
+            for (int i : arr) {
+                if (i > max) {
+                    max = i;
+                }
+            }
+            return max;
+        }
+
+        private static int findMin ( int[] arr){
+            int min = arr[0];
+
+            for (int i : arr) {
+                if (i < min) {
+                    min = i;
+                }
+            }
+            return min;
+        }
     }
 }
